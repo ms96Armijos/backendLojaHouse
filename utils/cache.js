ece7af98777ac3855d2057fe45189d7c1457e73a
ms96Armijos <ms96armijos@gmail.com>
2021-08-02 20:31:00 -0500

    cambiando de dirección ip

--- utils/cache.js
+++ utils/cache.js
@@ -2,7 +2,7 @@ const getExpeditiusCache = require('express-expeditious');
 
 const defaultOptions = {
     namespace: 'expresscache',
-    defaultTtl: '15 minute',
+    defaultTtl: '5 minute',
     statusCodeExpires: {
         404: '5 minute',
         500: 0
